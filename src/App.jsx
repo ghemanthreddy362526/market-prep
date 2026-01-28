3f90fc0f95c46d5cfb54b6781c001e81afd704c2
G Hemanth Reddy <ghemanthreddy@Gs-MacBook-Air.local>
2026-01-28 14:32:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { useRef } from "react";
 
 import Today from "./pages/Today";
 import Journal from "./pages/Journal";
@@ -7,19 +8,24 @@ import Archive from "./pages/Archive";
 
 function AnimatedRoutes() {
   const location = useLocation();
+  const nodeRef = useRef(null);
 
   return (
-    <TransitionGroup>
+    <TransitionGroup component={null}>
       <CSSTransition
         key={location.pathname}
+        nodeRef={nodeRef}
         classNames="page"
         timeout={300}
+        unmountOnExit
       >
-        <Routes location={location}>
-          <Route path="/" element={<Today />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/archive" element={<Archive />} />
-        </Routes>
+        <div ref={nodeRef}>
+          <Routes location={location}>
+            <Route path="/" element={<Today />} />
+            <Route path="/journal" element={<Journal />} />
+            <Route path="/archive" element={<Archive />} />
+          </Routes>
+        </div>
       </CSSTransition>
     </TransitionGroup>
   );
